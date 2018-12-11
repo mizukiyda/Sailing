9c34b0fe2ac4af60ef3336ec8bd82a253b7340fe
GM2180136\nwuser <mizuki060411@gmail.com>
2018-12-11 15:02:27 +0900

    [add]enemy.cpp
    とにかく書いた

--- Sorce/Enemy.cpp
+++ Sorce/Enemy.cpp
@@ -3,9 +3,9 @@
 #include"MAP.h"
 #include"Player.h"
 
-#define NUM 4						//敵の最大数
+#define ENEMY_MAX 4						//敵の最大数
 
-S_Enemy enemy[NUM];					//敵の配列
+S_Enemy enemy[ENEMY_MAX];					//敵の配列
 
 //for文用に使う変数
 int i, j, k;
@@ -31,6 +31,12 @@ int Player_Hit_Flg = false;
 int EnemyGyallaly[4];				//敵の画像の変数
 int EnemyShotGyallaly;				//敵の弾の画像の変数
 
+//enemyへの当たり判定
+int Enemy_Hit_Flg = false;
+
+//生きている状態を作る
+int OnActives = true;
+
 int Enemy_Init() {
 	//初期化処理
 
@@ -38,9 +44,9 @@ int Enemy_Init() {
 	LoadDivGraph("resource/Image/", 2, 2, 1, 11, 6, EnemyShotGyallaly);								//敵の弾の画像をロード
 
 
-	for (i = 0;i < 4;i++) {
-		enemy[i].x = ;					//MAPからエネミーの初期x座標をもらう
-		enemy[i].y = ;					//MAPからエネミーの初期y座標をもらう
+	for (i = 0;i < ENEMY_MAX;i++) {
+		enemy[i].x = MAP_Enemy_Pos_Init_x(i);					//MAPからエネミーの初期x座標をもらう
+		enemy[i].y = MAP_Enemy_Pos_Init_y(i);					//MAPからエネミーの初期y座標をもらう
 	}
 
 	player_x = ;						//playerのx座標を入れるための変数
@@ -57,11 +63,21 @@ int Enemy_MapDeta(int x, int y) {
 
 int Enemy_Dpct() {
 
-	enemy[NUM].ex = enemy[NUM].x;		//enemyの弾をEnemyのx座標に格納する
-	enemy[NUM].ey = enemy[NUM].y;		//enemyの弾をEnemyのy座標に格納する
+	enemy[ENEMY_MAX].ex = enemy[ENEMY_MAX].x;		//enemyの弾をEnemyのx座標に格納する
+	enemy[ENEMY_MAX].ey = enemy[ENEMY_MAX].y;		//enemyの弾をEnemyのy座標に格納する
 
-	//目の前が壁かどうかの判別をする（enemy専用Mapを見て判断する)
-	if (Enemy_MapDeta(enemy[NUM].x, enemy[NUM].y) == E_Object_Wall) {
+	Enemy_Hit_Flg = ;								//playerからもらったフラグを入れる変数
+
+	if (Enemy_Hit_Flg == true) {					//もし当たったなら				
+		OnActives = false;							//生きている状態をOFFにする
+	}
+
+	//目の前が壁かどうかの判別をする（enemy専用Mapを見て判断する) -> 目の前が道のとき
+	if (Enemy_MapDeta(enemy[ENEMY_MAX].x, enemy[ENEMY_MAX].y) == E_Object_Load) {
+
+	}
+	//目の前が壁かどうかの判別をする（enemy専用Mapを見て判断する) -> 目の前が壁のとき
+	if (Enemy_MapDeta(enemy[ENEMY_MAX].x, enemy[ENEMY_MAX].y) == E_Object_Wall) {
 
 		if (E_Drct_Up == true) {
 			Drct = E_Drct_Down;
@@ -112,17 +128,17 @@ int Enemy_Dpct() {
 		
 int Enemy_Attack_Left() {		//左に弾を打つ
 
-	enemy[NUM].ex -= 5;
+	enemy[ENEMY_MAX].ex -= 5;
 
-	if (enemy[NUM].ex == player_x && enemy[NUM].ey == player_y) {			//もしplayerに当たったらフラグをONにして、Playerに当たり判定を渡す
+	if (enemy[ENEMY_MAX].ex == player_x && enemy[ENEMY_MAX].ey == player_y) {			//もしplayerに当たったらフラグをONにして、Playerに当たり判定を渡す
 		Player_Hit_Flg = true;
 	}
 
-	if (enemy[NUM].ex == player_x && enemy[NUM].ey == player_y) {			//もしplayer弾とエネミー弾が当たったら消滅する
+	if (enemy[ENEMY_MAX].ex == player_x && enemy[ENEMY_MAX].ey == player_y) {			//もしplayer弾とエネミー弾が当たったら消滅する
 		Attack_Flg = false;
 	}
 
-	if (enemy[NUM].ex >= enemy[NUM].x - MAP_SIZE * 3) {						//もしenemyの弾がenemyのx座標を中心に3マスを超えたら、消滅する。
+	if (enemy[ENEMY_MAX].ex >= enemy[ENEMY_MAX].x - MAP_SIZE * 3) {						//もしenemyの弾がenemyのx座標を中心に3マスを超えたら、消滅する。
 		Attack_Flg = false;
 	}
 
@@ -131,17 +147,17 @@ int Enemy_Attack_Left() {		//左に弾を打つ
 
 int Enemy_Attack_Right() {		//右に弾を打つ
 
-	enemy[NUM].ex += 5;
+	enemy[ENEMY_MAX].ex += 5;
 
-	if (enemy[NUM].ex == player_x && enemy[NUM].ey == player_y) {			//もしplayerに当たったらフラグをONにして、Playerに当たり判定を渡す
+	if (enemy[ENEMY_MAX].ex == player_x && enemy[ENEMY_MAX].ey == player_y) {			//もしplayerに当たったらフラグをONにして、Playerに当たり判定を渡す
 		Player_Hit_Flg = true;
 	}
 
-	if (enemy[NUM].ex == player_x && enemy[NUM].ey == player_y) {			//もしplayer弾とエネミー弾が当たったら消滅する
+	if (enemy[ENEMY_MAX].ex == player_x && enemy[ENEMY_MAX].ey == player_y) {			//もしplayer弾とエネミー弾が当たったら消滅する
 		Attack_Flg = false;
 	}
 
-	if (enemy[NUM].ex >= enemy[NUM].x + MAP_SIZE * 3) {						//もしenemyの弾がenemyのx座標を中心に3マスを超えたら、消滅する。
+	if (enemy[ENEMY_MAX].ex >= enemy[ENEMY_MAX].x + MAP_SIZE * 3) {						//もしenemyの弾がenemyのx座標を中心に3マスを超えたら、消滅する。
 		Attack_Flg = false;
 	}
 
@@ -150,17 +166,17 @@ int Enemy_Attack_Right() {		//右に弾を打つ
 
 int Enemy_Attack_Down() {		//下に弾を打つ
 
-	enemy[NUM].ey -= 5;
+	enemy[ENEMY_MAX].ey -= 5;
 
-	if (enemy[NUM].ex == player_x && enemy[NUM].ey == player_y) {			//もしplayerに当たったらフラグをONにして、Playerに当たり判定を渡す
+	if (enemy[ENEMY_MAX].ex == player_x && enemy[ENEMY_MAX].ey == player_y) {			//もしplayerに当たったらフラグをONにして、Playerに当たり判定を渡す
 		Player_Hit_Flg = true;
 	}
 
-	if (enemy[NUM].ex == player_x && enemy[NUM].ey == player_y) {			//もしplayer弾とエネミー弾が当たったら消滅する
+	if (enemy[ENEMY_MAX].ex == player_x && enemy[ENEMY_MAX].ey == player_y) {			//もしplayer弾とエネミー弾が当たったら消滅する
 		Attack_Flg = false;
 	}
 
-	if (enemy[NUM].ey >= enemy[NUM].y - MAP_SIZE * 3) {						//もしenemyの弾がenemyのy座標を中心に3マスを超えたら、消滅する。
+	if (enemy[ENEMY_MAX].ey >= enemy[ENEMY_MAX].y - MAP_SIZE * 3) {						//もしenemyの弾がenemyのy座標を中心に3マスを超えたら、消滅する。
 		Attack_Flg = false;
 	}
 
@@ -169,17 +185,17 @@ int Enemy_Attack_Down() {		//下に弾を打つ
 
 int Enemy_Attack_Up() {			//上に弾を打つ
 
-	enemy[NUM].ey += 5;
+	enemy[ENEMY_MAX].ey += 5;
 
-	if (enemy[NUM].ex == player_x && enemy[NUM].ey == player_y) {			//もしplayerに当たったらフラグをONにして、Playerに当たり判定を渡す
+	if (enemy[ENEMY_MAX].ex == player_x && enemy[ENEMY_MAX].ey == player_y) {			//もしplayerに当たったらフラグをONにして、Playerに当たり判定を渡す
 		Player_Hit_Flg = true;
 	}
 
-	if (enemy[NUM].ex == player_x && enemy[NUM].ey == player_y) {			//もしplayer弾とエネミー弾が当たったら消滅する
+	if (enemy[ENEMY_MAX].ex == player_x && enemy[ENEMY_MAX].ey == player_y) {			//もしplayer弾とエネミー弾が当たったら消滅する
 		Attack_Flg = false;
 	}
 
-	if (enemy[NUM].ey >= enemy[NUM].y + MAP_SIZE * 3) {						//もしenemyの弾がenemyのy座標を中心に3マスを超えたら、消滅する。
+	if (enemy[ENEMY_MAX].ey >= enemy[ENEMY_MAX].y + MAP_SIZE * 3) {						//もしenemyの弾がenemyのy座標を中心に3マスを超えたら、消滅する。
 		Attack_Flg = false;
 	}
 
@@ -188,14 +204,16 @@ int Enemy_Attack_Up() {			//上に弾を打つ
 
 int Enemy_Draw() {
 	//描画処理
-	for (i = 0;i < 4;i++) {					
-		DrawGraph(enemy[i].x * MAP_SIZE + count_x, enemy[i].y * MAP_SIZE + count_y, EnemyGyallaly[0], true);				//enemyの描写
+	
+	for (i = 0;i < 4;i++) {
+		if (OnActives == true) {																								//もしenemyが生きているのなら
+			DrawGraph(enemy[i].x * MAP_SIZE + count_x, enemy[i].y * MAP_SIZE + count_y, EnemyGyallaly[0], true);				//enemyの描写
 
-		if (Attack_Flg = true) {																							//もし攻撃状態なら
-			DrawGraph(enemy[i].ex, enemy[i].ey, EnemyShotGyallaly, true);													//攻撃の描写								
+			if (Attack_Flg = true) {																							//もし攻撃状態なら
+				DrawGraph(enemy[i].ex, enemy[i].ey, EnemyShotGyallaly, true);													//攻撃の描写								
+			}
 		}
 	}
-
 	return 0;
 }
 
