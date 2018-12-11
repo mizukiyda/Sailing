842562808466240726f3964473fdd9659a352a18
ishikrua <ishikura.yda@gmail.com>
2018-12-11 21:33:19 +0900

    Revert "[update] マップの更新"
    This reverts commit c07b5ca41f25379ce8869f3f51e7f7af9639999f.

--- Sorce/MAP.cpp
+++ Sorce/MAP.cpp
@@ -1,4 +1,5 @@
 // マップ表示基本
+#include "string.h"
 #include "DxLib.h"
 #include "MAP.h"
 #include "Player.h"
@@ -77,15 +78,15 @@ int MAP_Init() {
 	char Handletmp[256];	// 結果的にこの名前のパスでファイルを開く
 	char flagtmp[256];		// MAP_〇…〇の部分
 
-	strcpy_s(MAPHandle, "resauce/MAP/MAP_");	// MAPHandleに固定文をコピー
+	strcpy(MAPHandle, "resauce/MAP/MAP_");	// MAPHandleに固定文をコピー
 
-	sprintf_s(flagtmp, "%d", Handleflag);		// falgtmpにMAP_SetHandleflagから数字を入れる
+	sprintf(flagtmp, "%d", Handleflag);		// falgtmpにMAP_SetHandleflagから数字を入れる
 
-	strcpy_s(Handletmp, MAPHandle);			// MAPHandleにコピーしたのをコピー
+	strcpy(Handletmp, MAPHandle);			// MAPHandleにコピーしたのをコピー
 
-	strcat_s(Handletmp, flagtmp);				// flagtmpにコピーしたのをコピー
+	strcat(Handletmp, flagtmp);				// flagtmpにコピーしたのをコピー
 
-	strcat_s(Handletmp, ".csv");				// 最後に.csvをコピー
+	strcat(Handletmp, ".csv");				// 最後に.csvをコピー
 
 	// resauce/MAP/MAP_ / %d / .csv	で区画分けしている
 
@@ -119,11 +120,11 @@ int MAP_Init() {
 	// ファイルを閉じる
 	FileRead_close(FileHandle);
 
-	strcpy_s(Handletmp, MAPHandle);
+	strcpy(Handletmp, MAPHandle);
 
-	strcat_s(Handletmp, flagtmp);
+	strcat(Handletmp, flagtmp);
 
-	strcat_s(Handletmp, ".txt");
+	strcat(Handletmp, ".txt");
 
 
 	
@@ -199,25 +200,33 @@ int MAP_Draw() {
 		for (j = 0; j < MAP_WIDTH; j++)
 		{
 
+			/*
+			typedef enum
+{
+	E_Object_Wall,      //行けない場所
+	E_Object_Load,      //歩ける場所
+	E_Object_Goal,      //ゴール
+}E_Object;
+			// プレイヤーヘッダーにを入れる
+
 			
-			if (MAP[i][j] == P_wall)		// 壁の描画処理
+			if (MAP[i][j] == E_Object_Wall)		// 壁の描画処理
 			{
-				//DrawEnemy(j * MAP_SIZE, i * MAP_SIZE,j * MAP_SIZE + MAP_SIZE, i * MAP_SIZE + MAP_SIZE,GetColor(0, 230, 0), TRUE);
-				DrawGraph(j * MAP_SIZE, i * MAP_SIZE, Image_Wall, TRUE);
+				DrawEnemy(j * MAP_SIZE, i * MAP_SIZE,j * MAP_SIZE + MAP_SIZE, i * MAP_SIZE + MAP_SIZE,GetColor(0, 230, 0), TRUE);
+				//DrawGraph(j * MAP_SIZE, i * MAP_SIZE, Image_Wall, TRUE);
 			}
 
-			if (MAP[i][j] == P_load)		// 床の描画処理
+			if (MAP[i][j] == E_Object_Load)		// 床の描画処理
 			{
-				//DrawEnemy(j * MAP_SIZE, i * MAP_SIZE,j * MAP_SIZE + MAP_SIZE, i * MAP_SIZE + MAP_SIZE,GetColor(122, 255, 122), TRUE);
-				DrawGraph(j * MAP_SIZE, i * MAP_SIZE, Image_Load, TRUE);
+				DrawEnemy(j * MAP_SIZE, i * MAP_SIZE,j * MAP_SIZE + MAP_SIZE, i * MAP_SIZE + MAP_SIZE,GetColor(122, 255, 122), TRUE);
+				//DrawGraph(j * MAP_SIZE, i * MAP_SIZE, Image_Load, TRUE);
 			}
-			if (MAP[i][j] == E_goal)		// ゴールの描画処理
+			if (MAP[i][j] == E_Object_Goal)		// ゴールの描画処理
 			{
-				//DrawEnemy(j * MAP_SIZE, i * MAP_SIZE,j * MAP_SIZE + MAP_SIZE, i * MAP_SIZE + MAP_SIZE,GetColor(122, 122, 255), TRUE);
-				DrawGraph(j * MAP_SIZE, i * MAP_SIZE, Image_Goal, TRUE);
+				DrawEnemy(j * MAP_SIZE, i * MAP_SIZE,j * MAP_SIZE + MAP_SIZE, i * MAP_SIZE + MAP_SIZE,GetColor(122, 122, 255), TRUE);
+				//DrawGraph(j * MAP_SIZE, i * MAP_SIZE, Image_Goal, TRUE);
 			}
-			
-			/*
+			*/
 			if (MAP[i][j] == 0)		// 壁の描画処理
 			{
 				DrawBox(j * MAP_SIZE, i * MAP_SIZE, j * MAP_SIZE + MAP_SIZE, i * MAP_SIZE + MAP_SIZE, GetColor(0, 230, 0), TRUE);
@@ -234,8 +243,6 @@ int MAP_Draw() {
 				DrawBox(j * MAP_SIZE, i * MAP_SIZE, j * MAP_SIZE + MAP_SIZE, i * MAP_SIZE + MAP_SIZE, GetColor(122, 122, 0), TRUE);
 				//DrawGraph(j * MAP_SIZE, i * MAP_SIZE, Image_Goal, TRUE);
 			}
-			*/
-			
 		}
 		
 	}
